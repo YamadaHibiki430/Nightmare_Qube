3a4bf69899ef48daf7f94dfafd300752f8ffe53a
さず 神原 <hibisyun430@gmail.com>
2019-08-09 11:29:58 +0900

    ファイルの場所を変更

---
+++ Assets/Script/Utility.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Utility : MonoBehaviour
+{
+    // Vector3 を Vector3Int に変換
+    public static Vector3Int ToVector3Int(Vector3 v)
+    {
+        return new Vector3Int(
+            Mathf.RoundToInt(v.x),
+            Mathf.RoundToInt(v.y),
+            Mathf.RoundToInt(v.z));
+    }
+
+    // Vector3 を Vector3Int に変換(切り捨て)
+    public static Vector3Int FloorToVector3Int(Vector3 v)
+    {
+        return new Vector3Int(
+            Mathf.FloorToInt(v.x),
+            Mathf.FloorToInt(v.y),
+            Mathf.FloorToInt(v.z));
+    }
+}
