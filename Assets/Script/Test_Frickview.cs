92801d644c6b1c7c8ccc381c1f29b14691d58d62
greatninjyagayboy810 <greatninjyagayboy810@gmail.com>
2019-08-29 12:38:13 +0900

    Frickスクリプトとテスト用受信Script

---
+++ Assets/Script/Test_Frickview.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class Test_Frickview : MonoBehaviour
+{
+    [SerializeField] private Frick _frick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _frick.OnFricked.Subscribe(_direction => 
+        {
+            Debug.Log("Frickview:フリック方向:" + _direction);
+        });
+    }
+}
