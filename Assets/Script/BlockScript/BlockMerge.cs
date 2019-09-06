185923f0eff432e0e603677a35505a2605d7d66d
さず 神原 <hibisyun430@gmail.com>
2019-09-06 13:55:22 +0900

    プレイヤーの当たり判定と移動を追加

--- Assets/Script/BlockScript/BlockMerge.cs
+++ Assets/Script/BlockScript/BlockMerge.cs
@@ -1,9 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UniRx;
 
 public class BlockMerge : MonoBehaviour
 {
+
+    // すべてのブロックの監視
+    private BlockManejar blockmanejar = null;
+
+    //
+    [SerializeField]
+    private PlayerUnionList playerunion_list = null;
+
+    
+
+    // ブロックをくっつける
+    private void UnionBlockCheck()
+    {
+        UnionCheckAndSet(Utility.ToVector3Int(Vector3.forward) + Utility.ToVector3Int(transform.position));
+        UnionCheckAndSet(Utility.ToVector3Int(Vector3.back) + Utility.ToVector3Int(transform.position));
+        UnionCheckAndSet(Utility.ToVector3Int(Vector3.right) + Utility.ToVector3Int(transform.position));
+        UnionCheckAndSet(Utility.ToVector3Int(Vector3.left) + Utility.ToVector3Int(transform.position));
+        UnionCheckAndSet(Utility.ToVector3Int(Vector3.up) + Utility.ToVector3Int(transform.position));
+        UnionCheckAndSet(Utility.ToVector3Int(Vector3.down) + Utility.ToVector3Int(transform.position));
+    }
+
+    // 再帰的にブロックをくっつける
+    void UnionCheckAndSet(Vector3Int vector)
+    {
+        Union block = (Union)blockmanejar.GetBlock(vector, Block.BLOCK_TYPE.UINON);
+        if (block == null) return; // Union 以外のブロックなら返す
+        //Debug.Log("UnionCheckAndSet: " + vector + ", " + block.name);
+
+        //合体
+        if (!block.is_union)
+        {
+            block.transform.parent = transform;
+            block.is_union = true;
+
+            //透明化ブロックの場合消す
+            if (blockmanejar.GetBlock(vector, Block.BLOCK_TYPE.GOST))
+            {
+                Material Cube = blockmanejar.BlockObject(vector, Block.BLOCK_TYPE.UINON).GetComponent<Renderer>().material;
+                Sequence alphaseq = DOTween.Sequence();
+                alphaseq.Append(DOTween.ToAlpha(() => Cube.color, color => Cube.color = color, 0.0f, 1.0f));
+                Debug.Log("よばれた");
+
+                //終了後の処理
+                alphaseq.OnComplete(() =>
+                {
+                    Sequence seq = DOTween.Sequence();
+                    //seq.Complete();
+                    seq.Append(DOTween.ToAlpha(() => Cube.color, color => Cube.color = color, 0.0f, 3.0f));
+                    seq.Append(DOTween.ToAlpha(() => Cube.color, color => Cube.color = color, 0.3f, 0.75f));
+                    seq.Append(DOTween.ToAlpha(() => Cube.color, color => Cube.color = color, 0.0f, 0.75f));
+                    seq.SetLoops(-1);
+                });
+            }
+
+            //Playerblocklistに追加
+            playerunion_list.AddBlock(block);
+        }
+
+        UnionCheckAndSet(new Vector3Int(vector.x - 1, vector.y, vector.z));
+        UnionCheckAndSet(new Vector3Int(vector.x + 1, vector.y, vector.z));
+        UnionCheckAndSet(new Vector3Int(vector.x, vector.y - 1, vector.z));
+        UnionCheckAndSet(new Vector3Int(vector.x, vector.y + 1, vector.z));
+        UnionCheckAndSet(new Vector3Int(vector.x, vector.y, vector.z - 1));
+        UnionCheckAndSet(new Vector3Int(vector.x, vector.y, vector.z + 1));
+    }
 
 
 }
---
+++ Assets/Script/Player/MoveCheck.cs
@@ -0,0 +1,447 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCheck : MonoBehaviour
+{
+
+    //
+    [SerializeField]
+    private PlayerUnionList playerunion_list = null;
+
+    // すべてのブロックの監視
+    [SerializeField]
+    private BlockManejar blockmanejar = null;
+
+    //前に移動できるか
+    bool ForwardMoveCheck()
+    {
+        var result = true;
+
+        List<Block> fowardBlocks = playerunion_list.GetForwardBlocks();
+        var forwardPos = playerunion_list.GetForwardPosition();
+        var topPos = playerunion_list.GetTopPosition();
+        var zMax = forwardPos.z + topPos.y;
+
+        var count = playerunion_list.PivotForwardBlockCount_LMove();
+        int addcount = 0;
+        if (count == 0)
+        {
+            addcount = 1;
+        }
+        else
+        {
+            addcount = 0;
+        }
+        if (fowardBlocks.Count > 0)
+        {
+            for (int i = 0; i < fowardBlocks.Count; i++)
+            {
+                //ユニオンの処理
+                Vector3 pos = fowardBlocks[i].transform.position;
+                var fowardPos = new Vector3(pos.x, 0.0f, pos.z + pos.y + addcount);
+                var vector = Utility.ToVector3Int(fowardPos);
+
+                var fowardPosDown = new Vector3(pos.x, -1.0f, zMax + addcount);
+                var vectorDown = Utility.ToVector3Int(fowardPosDown);
+                bool MoveCheckDown = blockmanejar.BlockCheck(vectorDown);
+                if (!MoveCheckDown)
+                {
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("下にブロックがないです");
+                }
+
+                var block = (Union)blockmanejar.GetBlock(vector);
+                if (block != null && block.is_union == false)
+                {
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("ブロックがあります");
+                }
+
+                Vector3 pos2 = transform.position;
+                var fowardPos2 = new Vector3(pos2.x, 0.0f, pos2.z + pos2.y + addcount);
+                var vector2 = Utility.ToVector3Int(fowardPos2);
+                var block2 = (Union)blockmanejar.GetBlock(vector2);
+                if (block2 != null && block2.is_union == false)
+                {
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("ブロックがあります");
+                }
+
+            }
+
+            if (fowardBlocks[0].Block_Position.z == transform.position.z)
+            {
+                Vector3 pos2 = transform.position;
+                var fowardPosDown2 = new Vector3(pos2.x, -1.0f, pos2.z + pos2.y + 1);
+                var vectorDown2 = Utility.ToVector3Int(fowardPosDown2);
+                bool MoveCheckDown2 = blockmanejar.BlockCheck(vectorDown2);
+                if (!MoveCheckDown2)
+                {
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("下にブロックがないです");
+                }
+            }
+        }
+        else
+        {
+            Vector3 pos2 = transform.position;
+            var fowardPos2 = new Vector3(pos2.x, 0.0f, pos2.z + pos2.y + addcount);
+            var vector2 = Utility.ToVector3Int(fowardPos2);
+
+            var block2 = (Union)blockmanejar.GetBlock(vector2);
+            if (block2 != null && block2.is_union == false)
+            {
+                result = false; // ブロックがあるので移動できない
+                Debug.Log("ブロックがあります");
+            }
+
+            var fowardPosDown2 = new Vector3(pos2.x, -1.0f, pos2.z + pos2.y + 1);
+            var vectorDown2 = Utility.ToVector3Int(fowardPosDown2);
+            bool MoveCheckDown2 = blockmanejar.BlockCheck(vectorDown2);
+            if (!MoveCheckDown2)
+            {
+                result = false; // ブロックがあるので移動できない
+                Debug.Log("下にブロックがないです");
+            }
+        }
+        return result;
+    }
+
+    //後ろに移動できるか
+    bool BackMoveCheck()
+    {
+        var result = true;
+        List<Block> backBlocks = playerunion_list.GetBackBlocks();
+        var backpos = playerunion_list.GetBackPosition();
+        var topPos = playerunion_list.GetTopPosition();
+        var zMin = -(backpos.z - topPos.y);
+
+        var count = playerunion_list.PivotBackBlockCount_LMove();
+        int addcount = 0;
+        if (count == 0)
+        {
+            addcount = 1;
+        }
+        else
+        {
+            addcount = 0;
+        }
+        if (backBlocks.Count > 0)
+        {
+
+            for (int i = 0; i < backBlocks.Count; i++)
+            {
+                //ユニオンの処理
+                Vector3 position = backBlocks[i].transform.position;
+                var backPos = new Vector3(position.x, 0.0f, position.z - position.y - addcount);
+                var vector = Utility.ToVector3Int(backPos);
+
+                var backPosDown = new Vector3(position.x, -1.0f, -zMin - addcount);
+                var vectorDown = Utility.ToVector3Int(backPosDown);
+                bool MoveCheckDown = blockmanejar.BlockCheck(vectorDown);
+                if (!MoveCheckDown)
+                {
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("下にブロックがないです");
+                }
+
+                var block = (Union)blockmanejar.GetBlock(vector);
+                if (block != null && block.is_union == false)
+                {
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("ブロックがあります");
+                }
+
+                Vector3 pos2 = transform.position;
+                var backPos2 = new Vector3(pos2.x, 0.0f, pos2.z - pos2.y - addcount);
+                var vector2 = Utility.ToVector3Int(backPos2);
+                var block2 = (Union)blockmanejar.GetBlock(vector2);
+                if (block2 != null && block2.is_union == false)
+                {
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("ブロックがあります");
+                }
+
+            }
+
+            if (backBlocks[0].Block_Position.z == transform.position.z)
+            {
+                Vector3 pos3 = transform.position;
+                var backPosDown2 = new Vector3(pos3.x, -1.0f, pos3.z - pos3.y - 1);
+                var vectorDown2 = Utility.ToVector3Int(backPosDown2);
+                bool MoveCheckDown2 = blockmanejar.BlockCheck(vectorDown2);
+                if (!MoveCheckDown2)
+                {
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("下にブロックがないです");
+                }
+            }
+        }
+        else
+        {
+            Vector3 pos2 = transform.position;
+            var backPos2 = new Vector3(pos2.x, 0.0f, pos2.z - pos2.y - addcount);
+            var vector2 = Utility.ToVector3Int(backPos2);
+
+            var backPosDown2 = new Vector3(pos2.x, -1.0f, pos2.z - pos2.y - 1);
+            var vectorDown2 = Utility.ToVector3Int(backPosDown2);
+            bool MoveCheckDown2 = blockmanejar.BlockCheck(vectorDown2);
+            if (!MoveCheckDown2)
+            {
+                result = false; // ブロックがあるので移動できない
+                Debug.Log("下にブロックがないです");
+            }
+
+            var block2 = (Union)blockmanejar.GetBlock(vector2);
+            if (block2 != null && block2.is_union == false)
+            {
+                result = false; // ブロックがあるので移動できない
+                Debug.Log("ブロックがあります");
+            }
+        }
+        return result;
+    }
+
+    //右に移動できるか
+    bool RightMoveCheck()
+    {
+        var result = true;
+        List<Block> rightBlocks = playerunion_list.GetRightBlocks();
+        var rightpos = playerunion_list.GetRightPosition();
+        var topPos = playerunion_list.GetTopPosition();
+        var xMax = rightpos.x + topPos.y;
+
+        var count = playerunion_list.PivotRightblockCount_LMove();
+        int addcount = 0;
+        if (count == 0)
+        {
+            addcount = 1;
+        }
+        else
+        {
+            addcount = 0;
+        }
+        if (rightBlocks.Count > 0)
+        {
+
+            for (int i = 0; i < rightBlocks.Count; i++)
+            {
+                //ユニオンの処理
+                Vector3 position = rightBlocks[i].transform.position;
+                var rightPos = new Vector3(position.x + position.y + addcount, 0.0f, position.z);
+                var vector = Utility.ToVector3Int(rightPos);
+                var block = (Union)blockmanejar.GetBlock(vector);
+
+                //移動方向の判定
+                if (block != null && block.is_union == false)
+                {
+                    //ユニオンの判定
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("ブロックがあります");
+                }
+                //ユニオン下の処理
+                var rightPosDown = new Vector3(xMax + addcount, -1.0f, position.z);
+                var vectorDown = Utility.ToVector3Int(rightPosDown);
+                bool MoveCheckDown = blockmanejar.BlockCheck(vectorDown);
+
+                if (!MoveCheckDown)
+                {
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("下にブロックがないです");
+                }
+
+                Vector3 pos2 = transform.position;
+                var rightPos2 = new Vector3(pos2.x + pos2.y + addcount, 0.0f, pos2.z);
+                var vector2 = Utility.ToVector3Int(rightPos2);
+                var block2 = (Union)blockmanejar.GetBlock(vector2);
+                if (block2 != null && block2.is_union == false)
+                {
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("ブロックがあります");
+                }
+
+
+            }
+
+            if (rightBlocks[0].Block_Position.x == transform.position.x)
+            {
+                Vector3 pos3 = transform.position;
+                var rightPosDown2 = new Vector3(pos3.x + pos3.y + 1, -1.0f, pos3.z);
+                var vectorDown2 = Utility.ToVector3Int(rightPosDown2);
+                bool MoveCheckDown2 = blockmanejar.BlockCheck(vectorDown2);
+                if (!MoveCheckDown2)
+                {
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("下にブロックがないです");
+                }
+            }
+        }
+        else
+        {
+            Vector3 pos2 = transform.position;
+            var rightPos2 = new Vector3(pos2.x + pos2.y + addcount, 0.0f, pos2.z);
+            var vector2 = Utility.ToVector3Int(rightPos2);
+
+            var rightPosDown2 = new Vector3(pos2.x + pos2.y + 1, -1.0f, pos2.z);
+            var vectorDown2 = Utility.ToVector3Int(rightPosDown2);
+            bool MoveCheckDown2 = blockmanejar.BlockCheck(vectorDown2);
+            if (!MoveCheckDown2)
+            {
+                result = false; // ブロックがあるので移動できない
+                Debug.Log("下にブロックがないです");
+            }
+
+            var block2 = (Union)blockmanejar.GetBlock(vector2);
+            if (block2 != null && block2.is_union == false)
+            {
+                result = false; // ブロックがあるので移動できない
+                Debug.Log("ブロックがあります");
+            }
+        }
+        return result;
+    }
+
+    //左に移動できるか
+    bool LeftMoveCheck()
+    {
+        var result = true;
+        List<Block> leftBlocks = playerunion_list.GetLeftBlocks();
+        var leftpos = playerunion_list.GetLeftPosition();
+        var topPos = playerunion_list.GetTopPosition();
+        var xMin = -(leftpos.x - topPos.y);
+
+        var count = playerunion_list.PivotLeftblockCount_LMove();
+        int addcount = 0;
+        if (count == 0)
+        {
+            addcount = 1;
+        }
+        else
+        {
+            addcount = 0;
+        }
+        if (leftBlocks.Count > 0)
+        {
+
+            for (int i = 0; i < leftBlocks.Count; i++)
+            {
+                //ユニオンの処理
+                Vector3 position = leftBlocks[i].transform.position;
+                var leftPos = new Vector3(position.x - position.y - addcount, 0.0f, position.z);
+                var vector = Utility.ToVector3Int(leftPos);
+
+                var leftPosDown = new Vector3(-xMin - addcount, -1.0f, position.z);
+                var vectorDown = Utility.ToVector3Int(leftPosDown);
+                bool MoveCheckDown = blockmanejar.BlockCheck(vectorDown);
+                if (!MoveCheckDown)
+                {
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("下にブロックがないです");
+                }
+
+                var block = (Union)blockmanejar.GetBlock(vector);
+                if (block != null && block.is_union == false)
+                {
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("ブロックがあります");
+                }
+
+                Vector3 pos2 = transform.position;
+                var leftPos2 = new Vector3(pos2.x - pos2.y - addcount, 0.0f, pos2.z);
+                var vector2 = Utility.ToVector3Int(leftPos2);
+
+                var block2 = (Union)blockmanejar.GetBlock(vector2);
+                if (block2 != null && block2.is_union == false)
+                {
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("ブロックがあります");
+                }
+
+            }
+            if (leftBlocks[0].Block_Position.x == transform.position.x)
+            {
+                Vector3 pos3 = transform.position;
+                var leftPosDown2 = new Vector3(pos3.x - pos3.y - 1, -1.0f, pos3.z);
+                var vectorDown2 = Utility.ToVector3Int(leftPosDown2);
+                bool MoveCheckDown2 = blockmanejar.BlockCheck(vectorDown2);
+                if (!MoveCheckDown2)
+                {
+                    result = false; // ブロックがあるので移動できない
+                    Debug.Log("下にブロックがないです");
+                }
+            }
+        }
+        else
+        {
+            Vector3 pos2 = transform.position;
+            var leftPos2 = new Vector3(pos2.x - pos2.y - addcount, 0.0f, pos2.z);
+            var vector2 = Utility.ToVector3Int(leftPos2);
+
+            var leftPosDown2 = new Vector3(pos2.x - pos2.y - 1, -1.0f, pos2.z);
+            var vectorDown2 = Utility.ToVector3Int(leftPosDown2);
+            bool MoveCheckDown2 = blockmanejar.BlockCheck(vectorDown2);
+            if (!MoveCheckDown2)
+            {
+                result = false; // ブロックがあるので移動できない
+                Debug.Log("下にブロックがないです");
+            }
+
+            var block2 = (Union)blockmanejar.GetBlock(vector2);
+            if (block2 != null && block2.is_union == false)
+            {
+                result = false; // ブロックがあるので移動できない
+                Debug.Log("ブロックがあります");
+            }
+        }
+        return result;
+    }
+
+    // 移動できるか
+    public bool Move_Check(Vector3 direction)
+    {
+        var result = false;
+
+        if (direction == Vector3.forward)
+        {
+            result = ForwardMoveCheck();
+        }
+        if (direction == Vector3.back)
+        {
+            result = BackMoveCheck();
+        }
+        if (direction == Vector3.right)
+        {
+            result = RightMoveCheck();
+        }
+        if (direction == Vector3.left)
+        {
+            result = LeftMoveCheck();
+        }
+        return result;
+    }
+
+    //移動先の高さを取得
+    private Vector3 SexyDynamiteMoveVector3(Vector3 direction)
+    {
+        var result = Vector3.zero;
+
+        if (direction == Vector3.forward)
+        {
+            result.y = playerunion_list.GetForwardPosition().z - playerunion_list.GetBackPosition().z;
+        }
+        if (direction == Vector3.back)
+        {
+            result.y = playerunion_list.GetForwardPosition().z - playerunion_list.GetBackPosition().z;
+        }
+        if (direction == Vector3.right)
+        {
+            result.y = playerunion_list.GetRightPosition().x - playerunion_list.GetLeftPosition().x;
+        }
+        if (direction == Vector3.left)
+        {
+            result.y = playerunion_list.GetRightPosition().x - playerunion_list.GetLeftPosition().x;
+        }
+        return result;
+    }
+}
